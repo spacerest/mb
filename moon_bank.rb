712b5d34428329b76d6cfd9fc0e04c1dd9133282
spacerest <robconvo@gmail.com>
2016-04-10 15:33:10 -0500

    added withdrawing/depositing capabilities

--- moon_bank.rb
+++ moon_bank.rb
@@ -19,21 +19,33 @@ get '/signup' do
 end
   
 post '/signup_confirm' do
-  @first_name = params['first_name']
-  @last_name = params['last_name']
-  @create_member = @bank.new_member([@first_name, @last_name])
-  @member = @bank.get_member(Session[:members].length)
-  Session[:members] << @member.access_name
-  Session[:member_id] = Session[:members].index(@member.access_name)
-  Session[@member.access_name] = []
-
-  erb :signup_confirm
+  if params['first_name'] && params['last_name']
+    @first_name = params['first_name']
+    @last_name = params['last_name']
+    @create_member = @bank.new_member([@first_name, @last_name])
+    @member = @bank.get_member(Session[:members].length)
+    Session[:members] << @member.access_name
+    Session[:member_id] = Session[:members].index(@member.access_name)
+    Session[@member.access_name] = []
+  elsif params[:change_member]
+    @member_access = params[:change_member]
+    Session[:member_id] = Session[:members].index(@member_access)
+  end
+    erb :signup_confirm
 end
 
 get '/accounts' do
 
 
   erb :accounts
+end
+
+post '/update_account' do
+  Session[:account_id] = params[:which_account].split("")[4..7].join.to_i
+  @action = params[:which_action]
+
+  erb @action.to_sym   
+
 end
 
 post '/new_account' do
@@ -47,17 +59,25 @@ end
 
 post '/show_account' do
   @deposit_amount = params['deposit_amount'].to_i
+  @withdraw_amount = params['withdraw_amount'].to_i
   @member = @bank.get_member(Session[:member_id])
   @account = @member.get_account(Session[:account_id])
 
   @account.deposit(@deposit_amount)
+  @account.withdraw(@withdraw_amount)
 
   erb :show_account
+end
+
+get '/members' do
+  
+  erb :members
 end
 
 Options = {
   'home' => '/',
   'new member' => '/signup',
+  'change member' => '/members',
   'manage accounts' => '/accounts'
 }
 
